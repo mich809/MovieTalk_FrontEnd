8e329f8e5a4a1ddf15593d4fb7d09a2aa8207834
Michael Caridad <caridadmichael@gmail.com>
2021-12-01 01:50:35 -0500

    added validation to login form

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,34 +1,73 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component } from '@angular/core';
 import { User } from 'src/app/Models/user';
 import {httpCalls} from "../../Services/httpCalls.service"
+import {Router} from "@angular/router"
+import { FormBuilder, Validators ,FormControl} from '@angular/forms';
+
+
+
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-    user = new User('','')    
+export class LoginComponent  { 
+  username = new FormControl('', [Validators.required ]);
+  password = new FormControl('', [Validators.required]);
+
+  hide = true;
+
+  loginForm = this.formBuilder.group({
+    username: this.username,
+    password: this.password
+  });
+
+
  
    
   
 
-  constructor(private httpcaller : httpCalls) { }
 
-  ngOnInit(): void {
-  }
+  constructor(private httpcaller : httpCalls , private router : Router , private formBuilder: FormBuilder,) { }
 
-  loginUser(){  
-        this.httpcaller.loginUser(this.user).subscribe(data=>{ 
-             
-        console.log(data.jwtToken)
+
+
+    loginUser() : void{   
+     let user = new User(this.loginForm.controls['username'].value,this.loginForm.controls['password'].value)   
+     this.httpcaller.loginUser(user).subscribe(data => {        
+         this.setSession(data.jwtToken)
         
-      })
+     },error => {
+        console.log('error')
+     },()=>{
+       this.router.navigateByUrl('/main/home')
+     }
+      )
   }
 
+  
+  private setSession(authResult : any) {       
+    localStorage.setItem('token', authResult);
+   
  
+    
+}  
+
+
+   
+      
+  }   
+       
+       
+       
+   
  
-  }
+  
+
+
+ 
+  
 
 
