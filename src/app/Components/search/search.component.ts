ed8afffa87d12cc677ab1a5646bf385b4621dd00
Michael Caridad <caridadmichael@gmail.com>
2021-12-01 01:55:08 -0500

    added search movie logic

--- src/app/Components/search/search.component.ts
+++ src/app/Components/search/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/Models/movie.model';
+import {APIcallerService} from "../../Services/apicaller.service"
+
 
 @Component({
   selector: 'app-search',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  movieTitle = ''
+  movies : Movie [] = [   
+  ]
 
-  constructor() { }
+
+  constructor(private caller : APIcallerService) { }
 
   ngOnInit(): void {
   }
 
-}
+  searchMovie(){   
+    this.movies = []
+    this.caller.searchMovie(this.movieTitle).subscribe(data =>{
+      this.movies = data.results   
+      this.movies.forEach((element,index)=>{     
+            if(element.poster_path == null && element.backdrop_path == null){             
+              this.movies.splice(index,1)
+            
+            }
+      })  
+    })}
+
+  searchImage(){
+    
+  }
+  
+  
+  
+  
+  
+  }
