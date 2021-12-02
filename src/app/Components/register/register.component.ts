5100706e45a27e5c4395dac654cda19f30f48d49
Michael Caridad <caridadmichael@gmail.com>
2021-12-02 16:17:03 -0500

    added ng material form instead

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component,OnInit } from '@angular/core';
 import { User } from 'src/app/Models/user';
 import {httpCalls} from "../../Services/httpCalls.service"
+import { FormBuilder, Validators ,FormControl} from '@angular/forms';
+import {passwordMatchValidator} from '../../Services/passwordMatchValidator'
  
 @Component({
   selector: 'app-register',
@@ -11,28 +13,45 @@ import {httpCalls} from "../../Services/httpCalls.service"
 
 
 export class RegisterComponent implements OnInit  {
-  user = new User('','')
-  confirmPassword : string = ''
-  test : any 
+  
+   username = new FormControl('', [Validators.required,Validators.minLength(4)]);
+   password = new FormControl('', [Validators.required,Validators.minLength(8)]);
+   confirmPassword = new FormControl('', [Validators.required,Validators.minLength(8) ]);
 
-  constructor(private httpcaller : httpCalls) { }
+  hide = true;
+
+ 
+  registrationForm = this.formBuilder.group({
+    username: this.username,
+    password: this.password,
+    confirmPassword : this.confirmPassword
+  });
+  
+
+
+
+
+  constructor(private httpcaller : httpCalls ,private formBuilder: FormBuilder) { }
 
   ngOnInit(): void{   
   }
   registerUser(){
-        if (this.confirmPassword != this.user.password){
+        if (this.registrationForm.controls['password'].value != this.registrationForm.controls['confirmPassword'].value){
           console.log('password does not match')
+         
+        }else{  
+          let user = new User(this.registrationForm.controls['username'].value,this.registrationForm.controls['password'].value)  
+          this.httpcaller.registerUser(user).subscribe(data =>{
+              console.log(typeof(data))
+
+          })
         }
-        else{
-          this.httpcaller.registerUser(this.user).subscribe(data =>{
-            if(data === true){
-              console.log('hell yeah')
-            }
-          },(err)=>{
-            console.log("unable to add user")
-          });
+        
+       
+        
           
-        }
+          
+        
   }
   
 
