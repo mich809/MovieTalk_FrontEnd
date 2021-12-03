dae024846e2fc4e4f058f752b7221475035dcf46
Michael Caridad <caridadmichael@gmail.com>
2021-12-03 00:38:09 -0500

    added more fields

--- src/app/Models/movie.model.ts
+++ src/app/Models/movie.model.ts
@@ -4,13 +4,19 @@ export class Movie {
     overview: string;
     poster_path: string;
     backdrop_path: string;
+    id : string
+    runtime : string
+    
 
-    constructor(original_title:string,release_date:string,overview:string,poster_path:string,backdrop_path:string){
+    constructor(original_title:string,release_date:string,overview:string,poster_path:string,backdrop_path:string , id : string , runtime : string){
         this.original_title = original_title;
         this.release_date = release_date;
         this.overview = overview;
         this.poster_path = poster_path;
         this.backdrop_path = backdrop_path;
+        this.id = id;
+        this.runtime = runtime
+       
     }
 
  
