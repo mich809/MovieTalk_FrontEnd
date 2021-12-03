2168d84b25f4a500b2cfa84e3b3572f917b0d4d8
Michael Caridad <caridadmichael@gmail.com>
2021-12-03 00:38:37 -0500

    added method to call a single movie

--- src/app/Services/apicaller.service.ts
+++ src/app/Services/apicaller.service.ts
@@ -2,28 +2,28 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class APIcallerService {
 
   apiKey = "86371abe86170fc1f5ead588458405b1"
-  base_url = "https://api.themoviedb.org/3"
+  base_url = "https://api.themoviedb.org/3/"
   image_url = "http://image.tmdb.org/t/p/w300/"
  
 
 
   constructor(private httpClient : HttpClient) { }
 
-  searchMovie(title:string) {    
+  searchMovies(title:string) {    
     let params = new HttpParams().set('api_key',this.apiKey).set('query',title).set('page',1);    
-    return this.httpClient.get<any>(this.base_url+"/search/movie",{params : params})
+    return this.httpClient.get<any>(this.base_url+"search/movie",{params : params})
   }
 
-  searchImage(path:String){
-    return this.httpClient.get<any>(this.image_url+path)
+  searchMovie(id:string){
+      let params = new HttpParams().set('api_key',this.apiKey).set('page',1);    
+      return this.httpClient.get<any>(this.base_url+'movie/'+id, {params: params})
+
   }
-
-  
-
 }
