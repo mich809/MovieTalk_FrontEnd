2aa12284e134e905d8603da51a15f135564c649d
Michael Caridad <caridadmichael@gmail.com>
2021-12-08 01:24:05 -0500

    added buttons and logic to show trailer and favorite movie

--- src/app/Components/movie-details/movie.component.ts
+++ src/app/Components/movie-details/movie.component.ts
@@ -1,7 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute} from '@angular/router';
 import {Movie} from "../../Models/movie.model";
 import {APIcallerService} from "../../Services/apicaller.service"
+import { TrailerComponent } from './trailer/trailer.component';
+import {MovieTrailer} from "../../Models/movie-trailer"
+import {DomSanitizer} from '@angular/platform-browser';
+import { CommentComponent } from './comment/comment.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import {httpCalls} from "../../Services/httpCalls.service"
+ 
+
+
 
 @Component({
   selector: 'app-movie',
@@ -10,21 +20,21 @@ import {APIcallerService} from "../../Services/apicaller.service"
 })
 export class MovieComponent implements OnInit {
   
-  movie !: Movie 
+  movie : Movie 
+  trailer : MovieTrailer
   status: boolean = false;
 
-  constructor(private activatedRoute : ActivatedRoute ,private caller : APIcallerService) {
-    
-   }
+  
+
+  constructor(private activatedRoute : ActivatedRoute ,private caller : APIcallerService, private dialog : MatDialog,private sanitizer : DomSanitizer,private backendCaller : httpCalls) {}
       
      
-  ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params =>{
+  ngOnInit(): void { 
+      this.activatedRoute.params.subscribe(params =>{
       const id = params['id']
       this.getMovie(id)
-     
-  })
-  }
+      this.getTrailer(id)
+      })}
 
 
   getMovie(id : string){
@@ -34,9 +44,49 @@ export class MovieComponent implements OnInit {
       })
   }
 
-  favoriteMovie(){   
+  getTrailer(id : string){
+    this.caller.getTrailer(id).subscribe(data => {
+      this.trailer = data.results[0]
+      this.trailer['url'] = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/' + this.trailer['key']);
+      
+
+   })
+  }
+
+
+  addToFavorites(){   
     this.status = !this.status;
-    console.log(this.status)
+   
+    if(this.status){
+      this.dialog.open(FavoritesComponent,{
+        data: this.movie.original_title + " has been added to your favorites."
+      }) 
+      this.saveMovietoBackend(this.movie)
+    }
+    else {
+      this.dialog.open(FavoritesComponent,{
+        data: this.movie.original_title + " has been removed from your favorites."
+      }) 
+      
+    }
+  }
+
+  watchTrailer(){   
+      this.dialog.open(TrailerComponent,{
+        data : this.trailer['url']
+
+      }) 
+   
+  }
+
+  postComment(){
+    this.dialog.open(CommentComponent) 
+  }
+
+  saveMovietoBackend(movie:Movie){
+    this.backendCaller.saveToFavorites(movie).subscribe(data =>{
+      console.log(data)
+    })
   }
 
 
