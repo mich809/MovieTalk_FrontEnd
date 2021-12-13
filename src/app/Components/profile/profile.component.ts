259bfebbd08a5d744198b459eec9897c09226975
Michael Caridad <caridadmichael@gmail.com>
2021-12-13 01:58:02 -0500

    profile shows favorite movies

--- src/app/Components/profile/profile.component.ts
+++ src/app/Components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/Models/movie.model';
+import {httpCalls} from "../../Services/backendClient"
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  favoriteMovies !: Movie[] 
 
-  constructor() { }
+  constructor(private backEndCaller : httpCalls) { }
 
   ngOnInit(): void {
+    this.favoriteMovies = []
+    this.backEndCaller.getFavorites().subscribe(data=>{
+      this.favoriteMovies = data
+    })
+
   }
 
 }
