16808903f18d83561661ff11b8f9fc48b3e64223
Michael Caridad <caridadmichael@gmail.com>
2021-12-13 01:54:15 -0500

    refactor some code

---
+++ src/app/Models/comment.model.ts
@@ -0,0 +1,10 @@
+export class Comment{
+     comment : string;
+	 postedBy : string;
+
+
+     constructor(comment:string , postedBy: string){
+         this.comment = comment;
+         this.postedBy = postedBy;
+     }
+}
